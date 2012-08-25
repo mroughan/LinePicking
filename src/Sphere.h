278bdf818eea7aed50bc9cb7d48e9174e5b8502f
lamestllama <eric@eparsonage.com>
2012-08-25 11:45:30 +0930

    Skeleton on Sphere Picking

---
+++ src/Sphere.h
@@ -0,0 +1,42 @@
+/* Sphere.h	
+ *
+ * Copyright 2012 Eric Parsonage <eric.parsonagen@adelaide.edu.au>
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *   
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef _SPHERE_H
+#define _SPHERE_H
+
+
+extern char * SphereDistanceName;
+extern char * SphereDistanceDescription;
+extern int SphereDistanceNpar;
+
+
+double SphereDistancePDF(double t, double* parameters);
+
+double SphereDistanceCDF(double a, double* b);
+
+double SphereDistanceMean(double* parameters);
+
+double SphereDistanceVar(double* parameters);
+
+void SphereDistanceSupport(double *t, double *parameters);
+
+void SphereDistanceCheckParameters(double *parameters, int *result, 
+                                char *error_str);
+
+#endif /* _SPHERE_H */
+
