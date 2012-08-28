3314c1849de3f79e09a9c8da48e6771afaefc578
Jonathan Tuke <jonotuke@gmail.com>
2012-08-29 08:53:40 +0930

    Changed R wrapper for LinePickingAllModes

--- R/LinePickingAllmodes.R
+++ R/LinePickingAllmodes.R
@@ -1,9 +1,18 @@
-#' Prints list of all modes
+# internal function to get number of non-empty entries
+GetLength <- function(x){
+  i <- min(which(x==""))
+  return(i)
+}
+#' Gets list of all modes
 #'
 #' Modes are integers describing possible geometries for the line picking
 #' problem. This function prints all possible modes.
 #' 
-#' @return prints out information
+#' @return List with 
+#' \itemize{
+#' \item names: list of the spaces names
+#' \item description: brief description of each space
+#' }
 #' @author Eric Parsonage, Matt Roughan, Jono Tuke
 #' @export
 #' @useDynLib LinePicking
@@ -11,6 +20,11 @@
 #' @examples
 #' LinePickingAllModes()
 LinePickingAllModes <- function(){
-  .C("LinePickingAllModes")
-  return()
+  tmp <- .C("LinePickingAllModes",
+            names = as.character(rep("",100)),
+            description = as.character(rep("",100)))
+  n <- GetLength(tmp$names) - 1
+  tmp$names <- tmp$names[1:n]
+  tmp$description <- tmp$description[1:n]
+  return(tmp)
 }
