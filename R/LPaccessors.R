5f6a345c544f2c5046496d69fb5491318c983380
Jonathan Tuke <jonotuke@gmail.com>
2012-08-29 13:30:44 +0930

    Some changes to ensure R check working

--- R/LPaccessors.R
+++ R/LPaccessors.R
@@ -1,10 +1,13 @@
-#' Get problem for \code{linepicking} object
+#' Get slot values for \code{linepicking} object
 #'
-#' Returns the problem for \code{linepicking} object rather than using @@
+#' Returns the slot value for \code{linepicking} object rather than using @@
 #'
 #' @param x A \code{linepicking} object
 #' @return value from slot
+#' @name accessors
 #' @export
+#' @docType methods
+#' @rdname accessor-methods
 #' @author Eric Parsonage, Matt Roughan, Jono Tuke
 #' @examples
 #' tmp <- CreateLinePicking(problem=0,para=10)
@@ -12,59 +15,36 @@
 setGeneric("problem",function(x) {
   standardGeneric("problem")
 })
+#' @rdname accessor-methods
+#' @aliases problem,linepicking-method
 setMethod("problem", "linepicking", function(x){
   slot(x,"problem")
 })
-#' Get para for \code{linepicking} object
-#'
-#' Returns the para for \code{linepicking} object rather than using @@
-#'
-#' @param x A \code{linepicking} object
-#' @return value from slot
-#' @export
-#' @author Eric Parsonage, Matt Roughan, Jono Tuke
-#' @examples
-#' tmp <- CreateLinePicking(problem=0,para=10)
-#' para(tmp)
-#'
+#' @rdname accessor-methods
+#' @aliases para
 setGeneric("para", function(x) {
   standardGeneric("para")
 })
+#' @rdname accessor-methods
+#' @aliases para,linepicking-method
 setMethod("para", "linepicking", function(x){
   slot(x,"parameters")
 })
-#' Get support for \code{linepicking} object
-#'
-#' Returns the support for \code{linepicking} object rather than using @@
-#'
-#' @param x A \code{linepicking} object
-#' @return value from slot
-#' @export
-#' @author Eric Parsonage, Matt Roughan, Jono Tuke
-#' @examples
-#' tmp <- CreateLinePicking(problem=0,para=10)
-#' support(tmp)
-#' para(tmp)
+#' @rdname accessor-methods
 setGeneric("support",function(x) {
   standardGeneric("support")
 })
+#' @rdname accessor-methods
+#' @aliases support,linepicking-method
 setMethod("support", "linepicking", function(x){
   slot(x,"support")
 })
-#' Gets mean and variance for \code{linepicking} object
-#'
-#' Returns the mean and variance for \code{linepicking} object rather than using @@
-#'
-#' @param x A \code{linepicking} object
-#' @return value from slot
-#' @export
-#' @author Eric Parsonage, Matt Roughan, Jono Tuke
-#' @examples
-#' tmp <- CreateLinePicking(problem=0,para=10)
-#' stats(tmp)
+#' @rdname accessor-methods
 setGeneric("stats",function(x) {
   standardGeneric("stats")
 })
+#' @rdname accessor-methods
+#' @aliases stats,linepicking-method
 setMethod("stats", "linepicking", function(x){
   tmp <- c(mean=slot(x,"mean"),
            var=slot(x,"var"))
@@ -78,6 +58,7 @@ setMethod("stats", "linepicking", function(x){
 #' @param t a distance
 #' @return probability density function 
 #' @export
+#' @rdname pdf-method
 #' @author Eric Parsonage, Matt Roughan, Jono Tuke
 #' @examples
 #' tmp <- CreateLinePicking(problem=0,para=10)
@@ -86,6 +67,8 @@ setMethod("stats", "linepicking", function(x){
 setGeneric("CalcPDF",function(x,t) {
   standardGeneric("CalcPDF")
 })
+#' @rdname pdf-method
+#' @aliases CalcPDF,linepicking-method
 setMethod("CalcPDF", "linepicking", function(x,t){
   tmp <- LinePickingPDF(t,problem(x),para(x))
   return(tmp)
@@ -98,6 +81,8 @@ setMethod("CalcPDF", "linepicking", function(x,t){
 #' @param t a distance
 #' @return cumulative density function
 #' @export
+#' @docType methods
+#' @rdname cdf-method
 #' @author Eric Parsonage, Matt Roughan, Jono Tuke
 #' @examples
 #' tmp <- CreateLinePicking(problem=0,para=10)
@@ -106,6 +91,8 @@ setMethod("CalcPDF", "linepicking", function(x,t){
 setGeneric("CalcCDF",function(x,t) {
   standardGeneric("CalcCDF")
 })
+#' @aliases CalcCDF CalcCDF,linepicking-method
+#' @rdname cdf-method
 setMethod("CalcCDF", "linepicking", function(x,t){
   tmp <- LinePickingCDF(t,problem(x),para(x))
   return(tmp)
--- inst/make_R_documentation.R
+++ inst/make_R_documentation.R
@@ -3,4 +3,5 @@ pkg <- as.package("~/Dropbox/LinePicking/")
 document(pkg)
 
 install(pkg)
+Sys.setenv(http_proxy="http://a1078881:bob4ever+2@www-proxy.adelaide.edu.au:8080")
 check(pkg)
