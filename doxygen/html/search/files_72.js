c62764a5588e44db52e26bbcc862756a0b271cc6
lamestllama <eric@eparsonage.com>
2012-08-31 05:20:16 +0930

    Updated reference manual to include new problem

--- doxygen/html/search/files_72.js
+++ doxygen/html/search/files_72.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['rectangle_2eh',['Rectangle.h',['../_rectangle_8h.html',1,'']]]
+  ['rectangle_2eh',['Rectangle.h',['../_rectangle_8h.html',1,'']]],
+  ['rectanglemanhattan_2eh',['RectangleManhattan.h',['../_rectangle_manhattan_8h.html',1,'']]]
 ];
--- doxygen/html/search/functions_72.js
+++ doxygen/html/search/functions_72.js
@@ -5,5 +5,11 @@ var searchData=
   ['rectangledistancemean',['RectangleDistanceMean',['../_rectangle_8h.html#ab705580ca1206e533ac91b91914edf9d',1,'Rectangle.c']]],
   ['rectangledistancepdf',['RectangleDistancePDF',['../_rectangle_8h.html#afa023c61f4035f333b56ba078da13329',1,'Rectangle.c']]],
   ['rectangledistancesupport',['RectangleDistanceSupport',['../_rectangle_8h.html#a066999772cca84018acd363d1995f1ab',1,'Rectangle.c']]],
-  ['rectangledistancevar',['RectangleDistanceVar',['../_rectangle_8h.html#aa95275ca8bf305ca44cc26abb62e38a7',1,'Rectangle.c']]]
+  ['rectangledistancevar',['RectangleDistanceVar',['../_rectangle_8h.html#aa95275ca8bf305ca44cc26abb62e38a7',1,'Rectangle.c']]],
+  ['rectanglemanhattandistancecdf',['RectangleManhattanDistanceCDF',['../_rectangle_manhattan_8h.html#adcd7815a45832662c0ba5b2733c19faa',1,'RectangleManhattan.c']]],
+  ['rectanglemanhattandistancecheckparameters',['RectangleManhattanDistanceCheckParameters',['../_rectangle_manhattan_8h.html#a1d30f47a9d0f75bfdef1a2f0d1de039b',1,'RectangleManhattan.c']]],
+  ['rectanglemanhattandistancemean',['RectangleManhattanDistanceMean',['../_rectangle_manhattan_8h.html#aee8e75a7c20fcd57d7e0973fe7405755',1,'RectangleManhattan.c']]],
+  ['rectanglemanhattandistancepdf',['RectangleManhattanDistancePDF',['../_rectangle_manhattan_8h.html#a0eb28f90684fb76ee37f6e1b63d1f54a',1,'RectangleManhattan.c']]],
+  ['rectanglemanhattandistancesupport',['RectangleManhattanDistanceSupport',['../_rectangle_manhattan_8h.html#a5d33e1aea01cc27b013c0291e19ed2ef',1,'RectangleManhattan.c']]],
+  ['rectanglemanhattandistancevar',['RectangleManhattanDistanceVar',['../_rectangle_manhattan_8h.html#a117ba0bca15232d5d7eb89b8b5a7c1ff',1,'RectangleManhattan.c']]]
 ];
