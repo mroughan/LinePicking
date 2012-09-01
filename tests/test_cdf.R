a06e1fd2b1cde1ed23bf7dc158719ffaf138fbd8
Jonathan Tuke <jonotuke@gmail.com>
2012-09-02 09:16:29 +0930

    Some test functions for R

--- tests/test_cdf.R
+++ tests/test_cdf.R
@@ -1,11 +1,8 @@
 library("LinePicking")
 problem <- 11
-para <- c(5,5)
+para <- c(4,5)
 integrate(LinePickingPDF,
           lower=LinePickingSupport(problem=problem,para=para)[1],
           upper=LinePickingSupport(problem=problem,para=para)[2],
           problem=problem, para=para)
-integrate(LinePickingPDF,
-          lower=0,
-          upper=10,
-          problem=problem, para=para)
+
