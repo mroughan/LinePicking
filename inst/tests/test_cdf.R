830f18f345a812dd4e8c0ecafca62385b37eece2
lamestllama <eric@eparsonage.com>
2012-09-10 02:04:17 +0930

    modified test_CDF.R to output a graph with the pdfs of all problems

--- inst/tests/test_cdf.R
+++ inst/tests/test_cdf.R
@@ -1,9 +1,12 @@
 library("LinePicking")
 
 context("Nothing as yet: TODO")
-# tmp <- LinePickingAllProblems()
-# for (i in 1:length(tmp)){
-#   parameters <- as.numeric(tmp[[i]]$parameters)
-#   support <- LinePickingSupport(problem=i,parameters=parameters)
-#   print(support)
-# }
+tmp <- LinePickingAllProblems()
+for (i in 1:length(tmp)){
+ problem <- i -1  
+ parameters<-as.numeric(tmp[[i]]$parameters)
+ support <- LinePickingSupport(problem=problem,parameters=parameters)
+ print(support)
+ pdf <- function(x) LinePickingPDF(x, problem, parameters)
+ curve(pdf, support[[1]], support[[2]], n = 101, add = TRUE, type = "l")
+}
